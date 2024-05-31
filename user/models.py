ffa977feed6fecd1ff0873de4c68ff214093c512
yashuv <yashuv.baskota1@gmail.com>
2024-05-31 22:22:55 +0545

    custom 'User' model creation subsituting default

--- user/models.py
+++ user/models.py
@@ -1,7 +1,92 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+import enum
+
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class GenderEnum(enum.Enum):
+    Male = "m"
+    Female = "f"
+    Other = "o"
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255)
+    gender = models.CharField(
+        max_length=1, choices=GenderEnum.choices(), 
+        default=GenderEnum.Male.value
+    )
+    password = models.CharField(max_length=500, null=False)
+    email = models.EmailField(max_length=255, null=False, unique=True)
+    phone = models.CharField(max_length=20, null=False)
+    dob = models.DateField(null=True, blank=True)
+    username = None
+    address = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions granted to each of their groups."
+        ),
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_set",
+        blank=True,
+        help_text=("Specific permissions for this user."),
+        related_query_name="user",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    # def clean(self):
+    #     super().clean()
+    #     self.email = self.__class__.objects.normalize_email(self.email)
+
+    def __str__(self):
+        return self.email
